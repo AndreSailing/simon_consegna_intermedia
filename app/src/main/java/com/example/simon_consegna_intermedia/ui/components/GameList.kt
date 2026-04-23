1dd97fadb649b23edc7e1727f02b72bc27f929af
AndreSailing <andrea.miolato@studenti.unipd.it>
2026-04-23 18:39:47 +0200

    DynamicList

--- app/src/main/java/com/example/simon_consegna_intermedia/ui/components/GameList.kt
+++ app/src/main/java/com/example/simon_consegna_intermedia/ui/components/GameList.kt
@@ -1,6 +1,5 @@
 package com.example.simon_consegna_intermedia.ui.components
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -24,22 +24,24 @@ import androidx.compose.ui.unit.dp
 fun GameList(modifier: Modifier,partite: ArrayList<String>){
     ConstraintLayout(modifier = modifier.fillMaxWidth()){
         val (col1)=createRefs()
-        Column(modifier= Modifier.constrainAs(col1){
+        LazyColumn(modifier= Modifier.constrainAs(col1){
             top.linkTo(parent.top)
             start.linkTo(parent.start)
-        }) {
-            for (i in 0 until partite.size step 1){
+            }) { items(partite.size) { i ->
+            for (i in 0 until partite.size step 1) {
                 Row(
-                    modifier= Modifier.fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth(),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    val quadratiPremuti=(partite[i].length+1)/2
-                    Text(text = quadratiPremuti.toString(),
+                    val quadratiPremuti = (partite[i].length + 1) / 2
+                    Text(
+                        text = quadratiPremuti.toString(),
                         style = MaterialTheme.typography.titleLarge,
                         color = MaterialTheme.colorScheme.primary
                     )
                     Spacer(Modifier.width(16.dp))
-                    Text(text = partite[i],
+                    Text(
+                        text = partite[i],
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis,
                         style = MaterialTheme.typography.titleLarge,
@@ -54,6 +56,7 @@ fun GameList(modifier: Modifier,partite: ArrayList<String>){
                     color = MaterialTheme.colorScheme.outlineVariant
                 )
 
+            }
             }
         }
 
