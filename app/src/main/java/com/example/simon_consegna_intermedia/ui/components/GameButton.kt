9a06ffa3130b6d63532c080a7e0ea63f3caf94b8
AndreSailing <andrea.miolato@studenti.unipd.it>
2026-04-27 14:52:19 +0200

    corrected typo

--- app/src/main/java/com/example/simon_consegna_intermedia/ui/components/GameButton.kt
+++ app/src/main/java/com/example/simon_consegna_intermedia/ui/components/GameButton.kt
@@ -9,9 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.simon_consegna_intermedia.R
 
 class GameButton (
